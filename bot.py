0ee7cb1ebe184fe0e918281d3ac375c39d989f6d
Brandon Birkholz <brandonjbirkholz@gmail.com>
2019-01-03 12:11:42 -0800

    Clean up reply

--- bot.py
+++ bot.py
@@ -14,23 +14,23 @@ async def on_ready():
     print('Ready!')
 
 
-def format_meaning(meaning):
-    reply = ''
+def format_meaning(word, meaning):
+    reply = f'**{word}**\n\n'
     for word_type, definitions in meaning.items():
         reply += f'*{word_type}*\n'
         counter = 1
         for definition in definitions:
-            reply += f'  {counter}. {definition}\n'
+            reply += f'    {counter}. {definition}\n'
             counter += 1
     return reply
 
 
 async def handle_message(message):
-    word = message.content[5:]
     try:
+        word = message.content[5:]
         meaning = PyDictionary.meaning(word)
         if meaning:
-            reply = format_meaning(meaning)
+            reply = format_meaning(word, meaning)
         else:
             reply = f'Sorry, {word} has no definition. :('
     except:
