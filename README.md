4733cc4ce62d7c3c7a5d77bbc477e9722e880b31
Brandon Birkholz <brandonjbirkholz@gmail.com>
2019-01-03 11:36:07 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dict-bot
+
+_A simple Discord bot that fetches definitions for words_
+
+### Commands
+
+`/def <word>` fetches the definition of `word` and returns it
---
+++ bot.py
@@ -0,0 +1,40 @@
+import asyncio
+import os
+
+import discord
+
+from PyDictionary import PyDictionary
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Ready!')
+
+
+def format_meaning(meaning):
+    reply = ''
+    for word_type, definitions in meaning.items():
+        reply += f'*{word_type}*\n'
+        counter = 1
+        for definition in definitions:
+            reply += f'  {counter}. {definition}\n'
+            counter += 1
+    return reply
+
+
+async def handle_message(message):
+    meaning = PyDictionary.meaning(message[5:])
+    reply = format_meaning(meaning)
+    await client.send_message(message.channel, reply)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/def '):
+        await handle_message(message)
+
+
+client.run(os.environ.get('DISCORD_TOKEN'))
